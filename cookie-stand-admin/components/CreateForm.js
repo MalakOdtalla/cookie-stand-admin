ba05eead73f4adf51418d0b14d0e996fb82c3962
MalakOdtalla <malakodt35@gmail.com>
2022-08-31 23:49:21 +0300

    add cookie stand

--- cookie-stand-admin/components/CreateForm.js
+++ cookie-stand-admin/components/CreateForm.js
@@ -1,72 +1,69 @@
 
-export default function Form(){
-    const initialValues = {
-        location: '',
-        max: 0,
-        min: 0,
-        avg: 0,
+import { useState } from "react";
+import useResource from "../Hooks/useResource";
+
+
+export default function Form(updateReport){
+    const [formItems, setFormItems] = useState({});
+    const { createResource } = useResource();
+  
+    const handleChange = (e) => {
+      let newItems = {
+        ...formItems,
+        [e.target.name]: e.target.value,
+      };
+      setFormItems(newItems);
     };
-    
-    const [values, setValues] = useState(initialValues);
-    
-    function submitHandler(event) {
-        event.preventDefault();
-        let newdata={
-    
-          location:event.target.location.value,
-          MinimumCustomersperHour:parseInt(event.target.MinimumCustomersperHour.value),
-          MaximumCustomersperHour:parseInt(event.target.MaximumCustomersperHour.value),
-          AverageCookiesperSale:parseFloat(event.target.AverageCookiesperSale.value),
-    
-        
-        };
-        setValues(newdata);
-    
-    }
-
-    return(
-        
-    <form className="flex flex-col p-6 mx-20 my-5 bg-green-300 rounded-md" onSubmit={submitHandler}>
-    <div className="mb-4 text-center">
-      <h2 className="text-xl">Create Cookie Stand</h2>
-    </div>
-    <div className="flex w-full mb-4">
-      <label className="pr-2">Location:</label>
-      <input
-        className="w-full"
-        name="location"
-        type="text"
-      
-      />
-    </div>
-    <div className="flex items-center gap-2 text-center">
-      <div className="bg-green-200 rounded-md ">
-        <label>Minimum Customers per Hour</label>
-        <input
-          name="minimum_customers_per_hour"
-          type="number"
-     
-        />
-      </div>
-      <div className="bg-green-200 rounded-md">
-        <label>Maximum Customers per Hour</label>
-        <input
-          name="maximum_customers_per_hour"
-          type="number"
-        
-        />
-      </div>
-      <div className="bg-green-200 rounded-md">
-        <label>Average Cookies per Sale</label>
-        <input
-          name="average_cookies_per_sale"
-          type="number"
-        
-        />
-      </div>
-      <button className="px-5 bg-green-400">Create</button>
-    </div>
-  </form>
-    )
-
-}
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      createResource(formItems);
+    };
+  
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col p-6 mx-20 my-5 bg-green-300 rounded-md"
+      >
+        <div className="mb-4 text-center">
+          <h2 className="text-xl">Create Cookie Stand</h2>
+        </div>
+        <div className="flex w-full mb-4">
+          <label className="pr-2">Location:</label>
+          <input
+            className="w-full"
+            name="location"
+            type="text"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex items-center gap-2 text-center">
+          <div className="bg-green-200 rounded-md ">
+            <label>Minimum Customers per Hour</label>
+            <input
+              name="minimum_customers_per_hour"
+              type="number"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="bg-green-200 rounded-md">
+            <label>Maximum Customers per Hour</label>
+            <input
+              name="maximum_customers_per_hour"
+              type="number"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="bg-green-200 rounded-md">
+            <label>Average Cookies per Sale</label>
+            <input
+              name="average_cookies_per_sale"
+              type="number"
+              onChange={handleChange}
+            />
+          </div>
+          <button className="px-5 bg-green-400">Create</button>
+        </div>
+      </form>
+    );
+  }
--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
+import Main from "../components/Main";
 
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 // import Head from '../components/Head'
 import Form from '../components/CreateForm'
+import LoginForm from "../components/LoginForm";
+
 
 
 export default function Home() {
@@ -19,12 +22,12 @@ export default function Home() {
 
       <Header/>
 
-      <main className="">
+      <main/>
 
         <Form/>
-       
+        (tokens) ? <Main /> : <LoginForm />
  
-      </main>
+      
      <Footer/>
 
     </div>
